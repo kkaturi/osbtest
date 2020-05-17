19025dbbe3ec7358fa6254743c7a8ba474f54a97
Kesava Katuri <kesava.katuri@oracle.com>
2020-05-17 20:51:08 +0530

    'add'

---
+++ HelloSBProject/init.cmd
@@ -0,0 +1,6 @@
+SET JAVA_HOME=E:\jdk1.8.0_144
+SET ANT_HOME=E:\OracleBPM12213\Middleware\Oracle_Home\oracle_common\modules\thirdparty\org.apache.ant\1.9.8.0.0\apache-ant-1.9.8
+SET M2_HOME=E:\OracleSOA1221\Middleware\Oracle_Home\oracle_common\modules\org.apache.maven_3.2.5
+SET M2=%M2_HOME%\bin
+SET ORACLE_HOME=E:\OracleBPM12213\Middleware\Oracle_Home
+SET PATH=%PATH%;%JAVA_HOME%\bin;%M2_HOME%\bin;%ANT_HOME%\bin
